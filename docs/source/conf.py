d866612e0df501bf56e1ef670cee217dd2c2f839
Sinesipho Matam <sinesiphomatam05@gmail.com>
2025-09-12 19:02:12 +0200

    Fix Sphinx configuration path for Django settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,34 +1,34 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+import django
+
+# -- Path setup --------------------------------------------------------------
+sys.path.insert(0, os.path.abspath("../.."))  
+os.environ["DJANGO_SETTINGS_MODULE"] = "student_management_system.settings"
+django.setup()
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'Student Management System'
-copyright = '2024, Sinesipho Matam'
-author = 'Sinesipho Matam'
-release = '00.00.02'
+project = "Student Management System"
+copyright = "2024, Sinesipho Matam"
+author = "Sinesipho Matam"
+release = "00.00.02"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",  
+    "sphinx.ext.napoleon",  
+]
 
-extensions = []
-
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
-
-
-import os 
-import sys
-sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] =  'student_management_system.settings'
+# Useful autodoc settings
+autodoc_member_order = "bysource"
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+}
